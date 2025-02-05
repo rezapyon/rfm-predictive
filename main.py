bcf8d15f89526bfd7c09be8a272a6c11bff05005
Rezapyon <rezapyon@gmail.com>
2025-02-05 19:02:30 +0700

    rfm prediction only

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import glob
 import os
 import shutil
 
-from pyspark.ml.classification import LogisticRegression
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, datediff, current_date, count, when, sum, max
+from pyspark.sql.functions import col, datediff, count, sum, max, lit
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.feature import VectorAssembler
 
@@ -23,7 +22,7 @@ if __name__ == "__main__":
     data = data.withColumn("FinalPrice", col("Quantity") * col("UnitPrice"))
 
     rfm_data = data.groupBy("CustomerID").agg(
-        (datediff(current_date(), max("InvoiceDate"))).alias("Recency"),
+        (datediff(lit("2012-01-01").cast("date"), max("InvoiceDate"))).alias("Recency"),
         count("InvoiceDate").alias("Frequency"),
         sum("FinalPrice").alias("Monetary"),
     )
@@ -32,28 +31,11 @@ if __name__ == "__main__":
     rfm_features = assembler.transform(rfm_data)
 
     # K-Means Clustering
-    kmeans = KMeans().setK(5).setSeed(1)
+    kmeans = KMeans().setK(4).setSeed(1)
     kmeans_model = kmeans.fit(rfm_features)
     rfm_data = kmeans_model.transform(rfm_features)
 
-    # criteria: If Recency > 30 days and Frequency < 2, label as churned
-    rfm_data = rfm_data.withColumn("IsChurn", when((col("Recency") > 30) & (col("Frequency") < 2), 1).otherwise(0))
-
-    churn_data = rfm_data.select("CustomerID", "Recency", "Frequency", "Monetary", "IsChurn")
-
-    # Vectorize features for logistic regression
-    assembler = VectorAssembler(inputCols=["Recency", "Frequency", "Monetary"], outputCol="features")
-    churn_features = assembler.transform(churn_data)
-
-    # Train logistic regression model
-    lr = LogisticRegression(featuresCol="features", labelCol="IsChurn")
-    lr_model = lr.fit(churn_features)
-
-    # Make predictions
-    predictions = lr_model.transform(churn_features)
-
-    # Store results in a CSV file for Superset
-    final_results = predictions.select("CustomerID", "Recency", "Frequency", "Monetary", "IsChurn", "prediction")
+    final_results = rfm_data.select("CustomerID", "Recency", "Frequency", "Monetary", "prediction")
 
     output_dir = "./final_result"
     final_results.write.mode("overwrite").csv(output_dir, header=True)
